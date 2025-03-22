9fbbc922538a5dc373b9f6232e4d86b8b168cf6a
flzroche <flzroche@gmail.com>
2025-03-22 18:57:39 +0900

    latast

--- lnCmds.sh
+++ lnCmds.sh
@@ -2,18 +2,18 @@
 
 cd $HOME
 
-rm -f ~/.config/starthip.toml
+rm -f ~/.config/starship.toml
 rm ~/.config/alacritty/alacritty.toml
 rm ~/.config/helix/config.toml
 rm .zshrc
-rm .gitfiles
+rm .gitconfig
 rm $HOME/Library/Application\ Support/espanso/match/addSnpt.yml
 rm ~/.tmux.conf
 
 ln -s ~/dotfilem/starship.toml ~/.config/starship.toml
 ln -s ~/dotfilem/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -s ~/dotfilem/.zshrc .zshrc
-ln -s ~/dotfilem/.gitfiles .gitfiles
+ln -s ~/dotfilem/.gitconfig .gitconfig
 ln -s ~/dotfilem/addSnpt.yml $HOME/Library/Application\ Support/espanso/match/addSnpt.yml
 ln -s ~/dotfilem/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfilem/helixconf.toml ~/.config/helix/config.toml
